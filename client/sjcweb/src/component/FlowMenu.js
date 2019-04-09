bc79c1e7affa368c3c01b3768bf19add76c5b2ff
tiago.albuquerque <tiagoamp@>
2019-04-09 10:12:48 -0300

    New client layout

--- client/sjcweb/src/component/FlowMenu.js
+++ client/sjcweb/src/component/FlowMenu.js
@@ -10,27 +10,27 @@ export default class FlowMenu extends Component {
         
           <div className="box">
             <div>
-                <i class="fa fa-files-o fa-2x" aria-hidden="true"></i>
+                <i className="fa fa-files-o fa-2x" aria-hidden="true"></i>
                 <span className="phaseName">Carregar Dados</span>
             </div>
             <p>Extrai dados dos arquivos no formato 'pdf' e gera as planilhas de entrada</p>            
           </div>
 
-          <i class="fa fa-angle-double-right fa-3x" aria-hidden="true"></i>
+          <i className="fa fa-angle-double-right fa-3x" aria-hidden="true"></i>
 
           <div className="box inactive">
             <div>
-                <i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+                <i className="fa fa-cogs fa-2x" aria-hidden="true"></i>
                 <span className="phaseName">Processar Dados</span>
             </div>
             <p>Processa dados das planilhas de entrada gerando realtório de processamento com erros e alertas</p>            
           </div>
 
-          <i class="fa fa-angle-double-right fa-3x" aria-hidden="true"></i>
+          <i className="fa fa-angle-double-right fa-3x" aria-hidden="true"></i>
 
           <div className="box inactive">
             <div>
-                <i class="fa fa-table fa-2x" aria-hidden="true"></i>
+                <i className="fa fa-table fa-2x" aria-hidden="true"></i>
                 <span className="phaseName">Gerar planilha de saída</span>
             </div>
             <p>Gera planilha de saída a partir dos dados de entrada carregados</p>            
