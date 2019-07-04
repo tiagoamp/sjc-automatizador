80716d8825f90b195414774a1d305f3397da22bc
tiago.albuquerque <tiagoamp@>
2019-07-04 08:43:12 -0300

    Add 'Avancar' button at the beginning of 'Converter' screen

--- client/sjcweb/src/component/steps/Converter.js
+++ client/sjcweb/src/component/steps/Converter.js
@@ -31,6 +31,7 @@ export default class Converter extends Component {
         <div>
           <button onClick={resetFiles}><i className="fa fa-trash-o fa-1x" aria-hidden="true"></i>LIMPAR</button>
           <button onClick={convertInputFiles}><i className="fa fa-file-pdf-o fa-1x" aria-hidden="true"></i>CONVERTER</button>
+          <button onClick={nextStep}>AVANÇAR<i className="fa fa-angle-double-right fa-1x" aria-hidden="true"></i></button>
         </div>
      
       </div>
