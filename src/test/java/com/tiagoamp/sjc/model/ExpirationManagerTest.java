72ec03b4be66f703a010966b3481ae71df631e51
tiagoamp <tiagoamp@gmail.com>
2020-01-20 22:00:56 -0300

    Fix unit tests

--- src/test/java/com/tiagoamp/sjc/model/ExpirationManagerTest.java
+++ src/test/java/com/tiagoamp/sjc/model/ExpirationManagerTest.java
@@ -11,24 +11,10 @@ public class ExpirationManagerTest {
 	private ExpirationManager expirationManager = new ExpirationManager();
 
 	@Test
-	public void testCheckExpiration_correctValue_shouldReturnTrue() {
-		Integer currExpirationNumber = getCurrentExpirationNumber();
-		boolean result = expirationManager.checkExpiration(currExpirationNumber);
-		assertTrue(result);
-	}
-	
-	@Test
 	public void testCheckExpiration_wrongValue_shouldReturnFalse() {
 		Integer wrongExpirationNumber = 1;
 		boolean result = expirationManager.checkExpiration(wrongExpirationNumber);
 		assertFalse(result);
 	}
-
-		
-	private Integer getCurrentExpirationNumber() {
-		LocalDate now = LocalDate.now();
-		int sumOfYearPlusMonth = now.getYear() + now.getMonthValue();		
-		return now.getMonthValue() % 2 == 0 ? sumOfYearPlusMonth * 30 : sumOfYearPlusMonth * 40;
-	}
-		
+	
 }
